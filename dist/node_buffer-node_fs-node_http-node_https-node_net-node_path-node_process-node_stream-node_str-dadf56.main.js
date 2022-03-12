588061bbb853752effe3a1db60b3e0c9c2f30ec3
keza681 <lindakeza19@gmail.com>
2022-03-12 19:31:15 +0200

    Add final touches

---
+++ dist/node_buffer-node_fs-node_http-node_https-node_net-node_path-node_process-node_stream-node_str-dadf56.main.js
@@ -0,0 +1,123 @@
+(self["webpackChunkldboard"] = self["webpackChunkldboard"] || []).push([["node_buffer-node_fs-node_http-node_https-node_net-node_path-node_process-node_stream-node_str-dadf56"],{
+
+/***/ "node:buffer":
+/*!*******************!*\
+  !*** node:buffer ***!
+  \*******************/
+/***/ (() => {
+
+throw new Error("Module build failed: UnhandledSchemeError: Reading from \"node:buffer\" is not handled by plugins (Unhandled scheme).\nWebpack supports \"data:\" and \"file:\" URIs by default.\nYou may need an additional plugin to handle \"node:\" URIs.\n    at C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:832:25\n    at Hook.eval [as callAsync] (eval at create (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:6:1)\n    at Object.processResource (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:829:8)\n    at processResource (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:220:11)\n    at iteratePitchingLoaders (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:171:10)\n    at runLoaders (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:397:2)\n    at NormalModule._doBuild (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:819:3)\n    at NormalModule.build (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:963:15)\n    at C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\Compilation.js:1371:12\n    at NormalModule.needBuild (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:1252:32)");
+
+/***/ }),
+
+/***/ "node:fs":
+/*!***************!*\
+  !*** node:fs ***!
+  \***************/
+/***/ (() => {
+
+throw new Error("Module build failed: UnhandledSchemeError: Reading from \"node:fs\" is not handled by plugins (Unhandled scheme).\nWebpack supports \"data:\" and \"file:\" URIs by default.\nYou may need an additional plugin to handle \"node:\" URIs.\n    at C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:832:25\n    at Hook.eval [as callAsync] (eval at create (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:6:1)\n    at Object.processResource (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:829:8)\n    at processResource (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:220:11)\n    at iteratePitchingLoaders (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:171:10)\n    at runLoaders (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:397:2)\n    at NormalModule._doBuild (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:819:3)\n    at NormalModule.build (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:963:15)\n    at C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\Compilation.js:1371:12\n    at NormalModule.needBuild (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:1252:32)");
+
+/***/ }),
+
+/***/ "node:http":
+/*!*****************!*\
+  !*** node:http ***!
+  \*****************/
+/***/ (() => {
+
+throw new Error("Module build failed: UnhandledSchemeError: Reading from \"node:http\" is not handled by plugins (Unhandled scheme).\nWebpack supports \"data:\" and \"file:\" URIs by default.\nYou may need an additional plugin to handle \"node:\" URIs.\n    at C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:832:25\n    at Hook.eval [as callAsync] (eval at create (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:6:1)\n    at Hook.CALL_ASYNC_DELEGATE [as _callAsync] (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\tapable\\lib\\Hook.js:18:14)\n    at Object.processResource (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:829:8)\n    at processResource (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:220:11)\n    at iteratePitchingLoaders (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:171:10)\n    at runLoaders (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:397:2)\n    at NormalModule._doBuild (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:819:3)\n    at NormalModule.build (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:963:15)\n    at C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\Compilation.js:1371:12");
+
+/***/ }),
+
+/***/ "node:https":
+/*!******************!*\
+  !*** node:https ***!
+  \******************/
+/***/ (() => {
+
+throw new Error("Module build failed: UnhandledSchemeError: Reading from \"node:https\" is not handled by plugins (Unhandled scheme).\nWebpack supports \"data:\" and \"file:\" URIs by default.\nYou may need an additional plugin to handle \"node:\" URIs.\n    at C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:832:25\n    at Hook.eval [as callAsync] (eval at create (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:6:1)\n    at Object.processResource (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:829:8)\n    at processResource (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:220:11)\n    at iteratePitchingLoaders (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:171:10)\n    at runLoaders (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:397:2)\n    at NormalModule._doBuild (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:819:3)\n    at NormalModule.build (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:963:15)\n    at C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\Compilation.js:1371:12\n    at NormalModule.needBuild (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:1252:32)");
+
+/***/ }),
+
+/***/ "node:net":
+/*!****************!*\
+  !*** node:net ***!
+  \****************/
+/***/ (() => {
+
+throw new Error("Module build failed: UnhandledSchemeError: Reading from \"node:net\" is not handled by plugins (Unhandled scheme).\nWebpack supports \"data:\" and \"file:\" URIs by default.\nYou may need an additional plugin to handle \"node:\" URIs.\n    at C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:832:25\n    at Hook.eval [as callAsync] (eval at create (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:6:1)\n    at Object.processResource (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:829:8)\n    at processResource (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:220:11)\n    at iteratePitchingLoaders (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:171:10)\n    at runLoaders (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:397:2)\n    at NormalModule._doBuild (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:819:3)\n    at NormalModule.build (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:963:15)\n    at C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\Compilation.js:1371:12\n    at NormalModule.needBuild (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:1252:32)");
+
+/***/ }),
+
+/***/ "node:path":
+/*!*****************!*\
+  !*** node:path ***!
+  \*****************/
+/***/ (() => {
+
+throw new Error("Module build failed: UnhandledSchemeError: Reading from \"node:path\" is not handled by plugins (Unhandled scheme).\nWebpack supports \"data:\" and \"file:\" URIs by default.\nYou may need an additional plugin to handle \"node:\" URIs.\n    at C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:832:25\n    at Hook.eval [as callAsync] (eval at create (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:6:1)\n    at Object.processResource (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:829:8)\n    at processResource (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:220:11)\n    at iteratePitchingLoaders (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:171:10)\n    at runLoaders (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:397:2)\n    at NormalModule._doBuild (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:819:3)\n    at NormalModule.build (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:963:15)\n    at C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\Compilation.js:1371:12\n    at NormalModule.needBuild (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:1252:32)");
+
+/***/ }),
+
+/***/ "node:process":
+/*!********************!*\
+  !*** node:process ***!
+  \********************/
+/***/ (() => {
+
+throw new Error("Module build failed: UnhandledSchemeError: Reading from \"node:process\" is not handled by plugins (Unhandled scheme).\nWebpack supports \"data:\" and \"file:\" URIs by default.\nYou may need an additional plugin to handle \"node:\" URIs.\n    at C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:832:25\n    at Hook.eval [as callAsync] (eval at create (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:6:1)\n    at Object.processResource (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:829:8)\n    at processResource (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:220:11)\n    at iteratePitchingLoaders (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:171:10)\n    at runLoaders (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:397:2)\n    at NormalModule._doBuild (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:819:3)\n    at NormalModule.build (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:963:15)\n    at C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\Compilation.js:1371:12\n    at NormalModule.needBuild (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:1252:32)");
+
+/***/ }),
+
+/***/ "node:stream":
+/*!*******************!*\
+  !*** node:stream ***!
+  \*******************/
+/***/ (() => {
+
+throw new Error("Module build failed: UnhandledSchemeError: Reading from \"node:stream\" is not handled by plugins (Unhandled scheme).\nWebpack supports \"data:\" and \"file:\" URIs by default.\nYou may need an additional plugin to handle \"node:\" URIs.\n    at C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:832:25\n    at Hook.eval [as callAsync] (eval at create (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:6:1)\n    at Object.processResource (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:829:8)\n    at processResource (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:220:11)\n    at iteratePitchingLoaders (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:171:10)\n    at runLoaders (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:397:2)\n    at NormalModule._doBuild (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:819:3)\n    at NormalModule.build (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:963:15)\n    at C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\Compilation.js:1371:12\n    at NormalModule.needBuild (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:1252:32)");
+
+/***/ }),
+
+/***/ "node:stream/web":
+/*!***********************!*\
+  !*** node:stream/web ***!
+  \***********************/
+/***/ (() => {
+
+throw new Error("Module build failed: UnhandledSchemeError: Reading from \"node:stream/web\" is not handled by plugins (Unhandled scheme).\nWebpack supports \"data:\" and \"file:\" URIs by default.\nYou may need an additional plugin to handle \"node:\" URIs.\n    at C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:832:25\n    at Hook.eval [as callAsync] (eval at create (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:6:1)\n    at Object.processResource (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:829:8)\n    at processResource (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:220:11)\n    at iteratePitchingLoaders (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:171:10)\n    at runLoaders (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:397:2)\n    at NormalModule._doBuild (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:819:3)\n    at NormalModule.build (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:963:15)\n    at C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\Compilation.js:1371:12\n    at NormalModule.needBuild (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:1252:32)");
+
+/***/ }),
+
+/***/ "node:url":
+/*!****************!*\
+  !*** node:url ***!
+  \****************/
+/***/ (() => {
+
+throw new Error("Module build failed: UnhandledSchemeError: Reading from \"node:url\" is not handled by plugins (Unhandled scheme).\nWebpack supports \"data:\" and \"file:\" URIs by default.\nYou may need an additional plugin to handle \"node:\" URIs.\n    at C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:832:25\n    at Hook.eval [as callAsync] (eval at create (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:6:1)\n    at Object.processResource (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:829:8)\n    at processResource (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:220:11)\n    at iteratePitchingLoaders (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:171:10)\n    at runLoaders (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:397:2)\n    at NormalModule._doBuild (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:819:3)\n    at NormalModule.build (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:963:15)\n    at C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\Compilation.js:1371:12\n    at NormalModule.needBuild (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:1252:32)");
+
+/***/ }),
+
+/***/ "node:util":
+/*!*****************!*\
+  !*** node:util ***!
+  \*****************/
+/***/ (() => {
+
+throw new Error("Module build failed: UnhandledSchemeError: Reading from \"node:util\" is not handled by plugins (Unhandled scheme).\nWebpack supports \"data:\" and \"file:\" URIs by default.\nYou may need an additional plugin to handle \"node:\" URIs.\n    at C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:832:25\n    at Hook.eval [as callAsync] (eval at create (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:6:1)\n    at Object.processResource (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:829:8)\n    at processResource (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:220:11)\n    at iteratePitchingLoaders (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:171:10)\n    at runLoaders (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:397:2)\n    at NormalModule._doBuild (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:819:3)\n    at NormalModule.build (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:963:15)\n    at C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\Compilation.js:1371:12\n    at NormalModule.needBuild (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:1252:32)");
+
+/***/ }),
+
+/***/ "node:zlib":
+/*!*****************!*\
+  !*** node:zlib ***!
+  \*****************/
+/***/ (() => {
+
+throw new Error("Module build failed: UnhandledSchemeError: Reading from \"node:zlib\" is not handled by plugins (Unhandled scheme).\nWebpack supports \"data:\" and \"file:\" URIs by default.\nYou may need an additional plugin to handle \"node:\" URIs.\n    at C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:832:25\n    at Hook.eval [as callAsync] (eval at create (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:6:1)\n    at Object.processResource (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:829:8)\n    at processResource (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:220:11)\n    at iteratePitchingLoaders (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:171:10)\n    at runLoaders (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:397:2)\n    at NormalModule._doBuild (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:819:3)\n    at NormalModule.build (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:963:15)\n    at C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\Compilation.js:1371:12\n    at NormalModule.needBuild (C:\\Users\\User\\Desktop\\LDboard\\node_modules\\webpack\\lib\\NormalModule.js:1252:32)");
+
+/***/ })
+
+}]);
